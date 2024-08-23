ac004571529d69c31b4085a7be908e83950c91d9
hiboherson <hiboherson@gmail.com>
2024-08-22 22:39:29 -0700

    initial commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
-# your code goes here!
+# lib/anagram.py
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, possible_anagrams):
+        sorted_word = ''.join(sorted(self.word.lower()))
+        return [word for word in possible_anagrams if ''.join(sorted(word.lower())) == sorted_word]
